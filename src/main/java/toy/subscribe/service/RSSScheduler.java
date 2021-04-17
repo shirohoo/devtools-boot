12338fd0c88368137a7aebf7a5d4ed7dbf4c6535
shirohoo <hch4102@naver.com>
2021-04-17 20:10:03 +0900

    refactor: 리팩토링
    target : 프론트
     - 프론트 디자인 변경
     - footer 추가
    Signed-off-by: shirohoo <hch4102@naver.com>

--- src/main/java/toy/subscribe/service/RSSScheduler.java
+++ src/main/java/toy/subscribe/service/RSSScheduler.java
@@ -5,24 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class RSSScheduler {
     
-    private final int DELAY = 1000 * 60 * 10; // 단위: ms
-    
-    private final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private final CollectPostService collectPostService;
     
+    private final int DELAY = 1000 * 60 * 10; // 단위: ms
+    
     @Scheduled(fixedRate = DELAY)
-    public void collect () throws Exception {
-        
-        log.info("[LOGGING : {}] Scheduler collect RSS !", timeFormatter.format(LocalDateTime.now()));
-        
+    public void collect() throws Exception {
+        log.info("[LOGGING] Scheduler collect RSS !");
         collectPostService.getAllGroupFeed();
     }
     
