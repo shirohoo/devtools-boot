c1bdec37aa3264ca959bf4da28a818080ce0f70d
shirohoo <hch4102@naver.com>
2021-04-21 11:06:50 +0900

    fix: 오늘 방문자 통계 쿼리 수정
     - 00시 00분 00초를 기준으로 비교하도록 변경
    
    Signed-off-by: shirohoo <hch4102@naver.com>

--- src/main/java/toy/subscribe/repository/RequestLogCustomRepositoryImpl.java
+++ src/main/java/toy/subscribe/repository/RequestLogCustomRepositoryImpl.java
@@ -18,15 +18,22 @@ public class RequestLogCustomRepositoryImpl extends QuerydslRepositorySupport im
     @Override
     @Transactional(readOnly = true)
     public Long getDau() {
-        
+    
         JPAQueryFactory queryFactory = new JPAQueryFactory(getEntityManager());
-        
+    
         return queryFactory
                 .select(requestLog.clientIp.countDistinct())
                 .from(requestLog)
-                .where(requestLog.regDate.gt(LocalDateTime.now().minusDays(1)))
+                .where(requestLog.regDate.gt(getOnTime()))
                 .fetchOne();
-        
+    
+    }
+    
+    private LocalDateTime getOnTime() {
+        return LocalDateTime.of(LocalDateTime.now().getYear(),
+                                LocalDateTime.now().getMonth(),
+                                LocalDateTime.now().getDayOfMonth(),
+                                0, 0, 0);
     }
     
 }
