dcc7dbaa44bb16b93dfeb41033d493dfe1cffbab
shirohoo <hch4102@naver.com>
2021-04-23 22:05:17 +0900

    fix: JSON Reader 테스트실패 수정
    Signed-off-by: shirohoo <hch4102@naver.com>

--- src/test/java/toy/subscribe/parser/JsonReaderTest.java
+++ src/test/java/toy/subscribe/parser/JsonReaderTest.java
@@ -5,9 +5,8 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,8 +19,7 @@ public class JsonReaderTest {
     @DisplayName("JSON_List<String>_읽기")
     public void readStringList() throws Exception {
         //given
-        File file = ResourceUtils.getFile("classpath:properties/propertiesFactory.json");
-        FileReader reader = new FileReader(file);
+        FileReader reader = new FileReader(new ClassPathResource("static/properties/propertiesFactory.json").getFile());
         JSONParser parser = new JSONParser();
         
         //when
@@ -38,8 +36,7 @@ public class JsonReaderTest {
     @DisplayName("JSON_List<Object>_읽기")
     public void readObjectList() throws Exception {
         //given
-        File file = ResourceUtils.getFile("classpath:properties/propertiesFactory.json");
-        FileReader reader = new FileReader(file);
+        FileReader reader = new FileReader(new ClassPathResource("static/properties/propertiesFactory.json").getFile());
         JSONParser parser = new JSONParser();
     
         //when
