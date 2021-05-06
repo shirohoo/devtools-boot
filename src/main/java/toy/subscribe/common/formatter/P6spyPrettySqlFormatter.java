e14fd6a3fab152b934593cee11b97877280e5228
shirohoo <hch4102@naver.com>
2021-05-07 01:00:57 +0900

    feat(master): 테스트 코드 작성
     - Job contents :
      1. Google RSS 호출 테스트 코드 작성
      2. P6Spy CustomFormatter 테스트 코드 작성
      3. WebClient 의존성 추가
    
    Signed-off-by: shirohoo <hch4102@naver.com>

--- src/main/java/toy/subscribe/common/formatter/P6spyPrettySqlFormatter.java
+++ src/main/java/toy/subscribe/common/formatter/P6spyPrettySqlFormatter.java
@@ -3,6 +3,7 @@ package toy.subscribe.common.formatter;
 import com.p6spy.engine.logging.Category;
 import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
 import org.hibernate.engine.jdbc.internal.FormatStyle;
+import org.thymeleaf.util.StringUtils;
 
 import java.util.Locale;
 
@@ -10,24 +11,28 @@ public class P6spyPrettySqlFormatter implements MessageFormattingStrategy {
     
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
-        return formatSql(category, sql);
+        return format(category, sql);
     }
     
-    private String formatSql(String category, String sql) {
-        if(sql == null || sql.trim().equals("")) {
+    private String format(String category, String sql) {
+        if(StringUtils.isEmptyOrWhitespace(sql.trim())) {
             return sql;
         }
         
         if(Category.STATEMENT.getName().equals(category)) {
             String s = sql.trim().toLowerCase(Locale.ROOT);
             if("create".startsWith(s) || "alter".startsWith(s) || "comment".startsWith(s)) {
-                sql = FormatStyle.DDL.getFormatter().format(sql);
+                sql = FormatStyle.DDL
+                        .getFormatter()
+                        .format(sql);
             }
             else {
-                sql = FormatStyle.BASIC.getFormatter().format(sql);
+                sql = FormatStyle.BASIC
+                        .getFormatter()
+                        .format(sql);
             }
         }
-        return "\n" + sql.toUpperCase() + "\n";
+        return "\n=============================================================" + sql.toUpperCase() + "\n=============================================================";
     }
     
 }
--- src/test/java/toy/subscribe/domain/board/parser/RSSFeedParserTest.java
+++ src/test/java/toy/subscribe/domain/board/parser/RSSFeedParserTest.java
@@ -1,9 +1,12 @@
 package toy.subscribe.domain.board.parser;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import toy.subscribe.domain.board.model.RSSFeed;
 
 import java.util.stream.Stream;
@@ -14,9 +17,9 @@ import static org.assertj.core.api.Assertions.assertThat;
  * 테스트가 실패하는 경우는 사이트의 RSS 피드 주소가 변하는 경우로 예상 됨
  */
 class RSSFeedParserTest {
-
+    
     @ParameterizedTest
-    @DisplayName("전사_RSS_파싱_테스트")
+    @DisplayName("전사_RSS_호출_파싱_테스트")
     @MethodSource("provideFeedUrlAndKeyword")
     public void readAllGroup(String url, String keyword) throws Exception {
         RSSFeedParser parser = new RSSFeedParser(url);
@@ -40,7 +43,25 @@ class RSSFeedParserTest {
                 Arguments.of("https://yanolja.github.io/feed.xml", "yanolja"),
                 Arguments.of("https://engineering.linecorp.com/ko/feed/", "line"),
                 Arguments.of("https://helloworld.kurly.com/feed.xml", "thefarmersfront")
-        );
+                        );
     }
-
+    
+    @Test
+    @DisplayName("Google_RSS_호출_테스트")
+    public void invokeGoogleApi() throws Exception {
+        // given
+        WebClient.RequestHeadersSpec<?> invoke = WebClient.builder().codecs(configurer->configurer.defaultCodecs()
+                                                                                                  .maxInMemorySize(16 * 1024 * 1024)
+                                                                           )
+                                                          .build()
+                                                          .get()
+                                                          .uri("https://www.blogger.com/feeds/456720922432942888/posts/default");
+        
+        // when
+        Mono<String> stringMono = invoke.retrieve().bodyToMono(String.class);
+        
+        // then
+        assertThat(stringMono).isNotNull();
+    }
+    
 }
