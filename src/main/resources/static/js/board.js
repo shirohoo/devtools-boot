6012123173e21ccc9bb3ea44fb3c3e88e7368de8
shirohoo <hch4102@naver.com>
2021-05-09 23:45:30 +0900

    master: Vue.js
     - Job contents :
      1. board.html & board.js Vue.js 관련 리팩토링 완료
    
     - Why did you do it? :
      1. Vue.js 학습 - 양방향 바인딩 본격 적용
      2. 클린코드 지향
    
    Signed-off-by: shirohoo <hch4102@naver.com>

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,45 +1,45 @@
 'use strict';
-
 const app = new Vue({
-	                    el  : '#app',
-	                    data: {
-		                    CONTEXT : '',
-		                    response: {
-			                    pages           : '',
-			                    visitorsOfReduce: '',
-			                    visitorsOfDay   : ''
+	                    el     : '#app',
+	                    data   : {
+		                    search          : {
+			                    page   : 0,
+			                    size   : 10,
+			                    company: '',
+			                    title  : ''
 		                    },
-		                    pager   : {},
+		                    contents        : {},
+		                    pager           : {},
+		                    visitorsOfReduce: 0,
+		                    visitorsOfDay   : 0
 	                    },
-
 	                    mounted: function(){
-		                    let url = '/boards?';
-		                    let param = 'page=0&size=10';
-		                    this.apiFetch(url, param, this.callbackDataBinding);
+		                    this.$nextTick(function(){
+			                    this.findContents();
+		                    });
 	                    },
-
 	                    methods: {
-		                    apiFetch(url, param, callback){
-			                    fetch(url + param)
+		                    findContents(page){
+			                    if(page !== undefined) this.search.page = page;
+			                    let query = Object.keys(this.search)
+			                                      .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(this.search[k]))
+			                                      .join('&');
+			                    let url = '/boards?' + query;
+			                    fetch(url)
 				                    .then(res => {if(res.status === 200) return res.json()})
-				                    .then(data => callback(data))
+				                    .then(data => {
+					                    this.contents = data.pages.content;
+					                    this.visitorsOfReduce = data.visitorsOfReduce;
+					                    this.visitorsOfDay = data.visitorsOfDay;
+					                    this.pager = this.setPage(data.pages);
+				                    })
 				                    .catch(() => alert('400, Bad Request'));
 		                    },
-		                    callbackDataBinding(data){
-			                    this.response.pages = data.pages;
-			                    this.response.visitorsOfReduce = data.visitorsOfReduce;
-			                    this.response.visitorsOfDay = data.visitorsOfDay;
-			                    this.pager = this.setPage(data.pages.pageable.pageNumber,
-			                                              data.pages.size,
-			                                              data.pages.totalElements,
-			                                              data.pages.totalPages);
-		                    },
-		                    setPage(currentPage, size, totalElements, totalPages){
-			                    let endPage = Math.ceil((currentPage + 1) / 10.0) * 10 - 1;
+		                    setPage(pages){
+			                    let endPage = Math.ceil((pages.pageable.pageNumber + 1) / 10.0) * 10 - 1;
 			                    let startPage = endPage - 9;
-
-			                    if(totalPages <= endPage){
-				                    endPage = totalPages - 1;
+			                    if(pages.totalPages <= endPage){
+				                    endPage = pages.totalPages - 1;
 			                    }
 
 			                    let index = [];
@@ -49,53 +49,27 @@ const app = new Vue({
 
 			                    return {
 				                    index        : index,
-				                    size         : size,
-				                    currentPage  : currentPage,
-				                    totalPages   : totalPages,
-				                    totalElements: totalElements
+				                    size         : pages.size,
+				                    first        : pages.first,
+				                    last         : pages.last,
+				                    currentPage  : pages.pageable.pageNumber,
+				                    totalPages   : pages.totalPages,
+				                    totalElements: pages.totalElements
 			                    };
-		                    },
-		                    enterKeyUp(){
-			                    if(event.keyCode == 13){
-				                    this.search();
-			                    }
 		                    },
 		                    formatNumber(num){
 			                    let regexp = /\B(?=(\d{3})+(?!\d))/g;
 			                    return num.toString().replace(regexp, ',');
 		                    },
-		                    resetSearchForm(){
-			                    document.querySelector('#searchForm').reset();
-			                    document.querySelector('#company').value = '';
-			                    this.search();
-		                    },
-		                    selectedCompany(){
-			                    let company = document.querySelector('#company');
-			                    let selectCompany = document.querySelector('#selectCompany');
-			                    company.value = selectCompany.value;
-			                    this.search();
-		                    },
-		                    search(){
-			                    let url = `${this.CONTEXT}/boards?`;
-			                    let param = new URLSearchParams();
-			                    let searchConditions = Array.from(
-				                    document.querySelector('#searchForm')
-				                            .getElementsByTagName('input')
-			                    ).filter((element) => {
-				                    return element.id != '';
-			                    });
-
-			                    for(const conditions of searchConditions){
-				                    param.append(conditions.name, conditions.value);
+		                    enterKeyUp(){
+			                    if(event.keyCode === 13){
+				                    this.findContents();
 			                    }
-
-			                    let currentPage = document.querySelector('#currentPageNum').value == '' ? 0 : document.querySelector('#currentPageNum').value;
-			                    let size = document.querySelector('#renderingCount').value;
-
-			                    param += param == '' ? '' : '&';
-			                    param += `page=${currentPage}&size=${size}`;
-
-			                    this.apiFetch(url, param, this.callbackDataBinding);
+		                    },
+		                    resetSearchForm(){
+			                    this.search.company = '';
+			                    this.search.title = '';
+			                    this.findContents();
 		                    },
 		                    isBetweenDay(regDate){
 			                    let regDateArray = String(regDate).split('-');
@@ -104,26 +78,6 @@ const app = new Vue({
 			                    let nowDate = new Date();
 			                    let betweenDay = Math.floor((nowDate.getTime() - date.getTime()) / 1000 / 60 / 60 / 24);
 			                    return betweenDay === 0;
-		                    },
-		                    pageMove(targetPage, size){
-			                    let url = `${this.CONTEXT}/boards?`;
-			                    let param = new URLSearchParams();
-
-			                    let searchConditions = Array.from(
-				                    document.querySelector('#searchForm')
-				                            .getElementsByTagName('input')
-			                    ).filter((element) => {
-				                    return element.id != '';
-			                    });
-
-			                    for(const ele of searchConditions){
-				                    param.append(ele.name, ele.value);
-			                    }
-
-			                    param += param == '' ? '' : '&';
-			                    param += `page=${targetPage}&size=${size}`;
-
-			                    this.apiFetch(url, param, this.callbackDataBinding);
 		                    },
 	                    },
                     });
