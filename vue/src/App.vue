4a2affaa1d12f26213243dfb79d4b3e4d52751c3
siro <hch4102@naver.com>
2021-06-08 22:51:50 +0900

    feat(3.0): Design App.vue
      - App.vue layout configuration
    
    Signed-off-by: siro <hch4102@naver.com>

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,55 +1,90 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-item router-link :to="{name: 'blog'}" exact>
+          <v-list-item-action>
+            <v-icon>fas fa-arrow-alt-circle-right</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Tech Blogs</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item router-link :to="{name: 'bookmark'}" exact>
+          <v-list-item-action>
+            <v-icon>fas fa-arrow-alt-circle-right</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Bookmarks</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item router-link :to="{name: 'dictionary'}" exact>
+          <v-list-item-action>
+            <v-icon>fas fa-arrow-alt-circle-right</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Open Source Dictionary</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item router-link :to="{name: 'manager'}" exact>
+          <v-list-item-action>
+            <v-icon>fas fa-lock</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Manager</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>DEVELOPMENT DICTIONARY</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <v-container class="fill-height" fluid>
+        <router-view></router-view>
+      </v-container>
     </v-main>
+
+    <v-footer dark padless>
+      <v-card class="flex" flat tile>
+        <v-card-title class="teal">
+          <strong class="subheading">🐱‍🏍 No one can do it for me, Only I can change my life.</strong>
+
+          <v-spacer></v-spacer>
+
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon href="https://github.com/shirohoo/development-dictionary" target="_blank">
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-title>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
+import Vuetify from "vuetify";
 
 export default {
-  name: 'App',
+  name   : 'App',
+  vuetify: new Vuetify(),
+  props  : {
+    source: String,
+  },
 
   data: () => ({
-    //
+    drawer: null,
+    icons : [
+      'mdi-github',
+    ],
   }),
+
+  created(){
+    this.$vuetify.theme.dark = true
+  },
 };
 </script>
