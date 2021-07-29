5569c87f66ab0906788215d066fc7babf4b6f8ba
shirohoo <hch4102@naver.com>
2021-07-29 22:35:54 +0900

    refactor(orm): Add JpaConfig, QuerydslConfig

--- src/main/java/toy/subscribe/Application.java
+++ src/main/java/toy/subscribe/Application.java
@@ -2,21 +2,12 @@ package toy.subscribe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-    
-    @Bean // JPA Exception → Spring Exception 으로 변환
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-        return new PersistenceExceptionTranslationPostProcessor();
     }
 }
