1793b5f09fc714a49747f24df31cc54293ae1b16
shirohoo <hch4102@naver.com>
2021-07-29 22:38:45 +0900

    refactor(scheduler): Modify RssSchedulerConfig

--- src/main/java/toy/subscribe/configs/scheduler/RssSchedulerConfig.java
+++ src/main/java/toy/subscribe/configs/scheduler/RssSchedulerConfig.java
@@ -1,19 +1,19 @@
 package toy.subscribe.configs.scheduler;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import toy.subscribe.domain.board.service.CollectPostService;
 
-@Slf4j
-@Service
+@Configuration
+@EnableScheduling
 @RequiredArgsConstructor
-public class RSSScheduler {
-    private final CollectPostService collectPostService;
-
+public class RssSchedulerConfig {
     private static final int COLLECT_DELAY = 1000 * 60 * 1; // 단위: ms
+
+    private final CollectPostService collectPostService;
 
     @Transactional
     @Scheduled(fixedRate = COLLECT_DELAY)
