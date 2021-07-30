af6fb68d7e7213acc0cd7e69b50b1042a7915b62
shirohoo <hch4102@naver.com>
2021-07-31 02:08:45 +0900

    refactor(security): Update Manager

--- src/main/java/toy/subscribe/configs/security/Manager.java
+++ src/main/java/toy/subscribe/configs/security/Manager.java
@@ -1,22 +1,29 @@
 package toy.subscribe.configs.security;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import toy.subscribe.configs.model.BaseEntity;
 
 import javax.persistence.*;
-import java.io.Serializable;
 
 @Entity
 @Getter
-@ToString
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Manager implements Serializable {
-    private static final long serialVersionUID = -1876574821445975508L;
-
-    @Column(name = "manager_id")
+public class Manager extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "manager_id")
     private Long id;
     private String username;
     private String password;
     private String authority;
+
+    @Builder(access = AccessLevel.PUBLIC)
+    private Manager(Long id, String username, String password, String authority) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authority = authority;
+    }
 }
