53de010a273b5edbba5fdfa79bb5bfba95065e9f
shirohoo <shirohoo4102@gmail.com>
2022-01-15 19:33:32 +0900

    :art: 테스트 패키지 컨텍스트 로딩 추가
    - 컨텍스트 재사용을 위함

--- src/test/java/toy/subscribe/ApplicationTest.java
+++ src/test/java/toy/subscribe/ApplicationTest.java
@@ -1,14 +1,9 @@
 package toy.subscribe;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ApplicationTest {
-    @Test
-    @DisplayName("스프링부트 컨텍스트가 제대로 올라가는지 테스트한다")
-    @Disabled(value = "테스트 시간이 너무 오래 걸려 비활성화")
-    public void main() {
-        Application.main(new String[]{});
+    public void contextLoads() {
     }
 }
