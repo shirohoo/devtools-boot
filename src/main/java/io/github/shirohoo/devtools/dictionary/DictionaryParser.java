b4f176e8563426fab7a9869f9ea62dd98e43a9ae
shirohoo <shirohoo4102@gmail.com>
2022-01-23 14:03:20 +0900

    :recycle: 사전생성기 리팩토링
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>

--- src/main/java/io/github/shirohoo/devtools/dictionary/DictionaryParser.java
+++ src/main/java/io/github/shirohoo/devtools/dictionary/DictionaryParser.java
@@ -2,6 +2,7 @@ package io.github.shirohoo.devtools.dictionary;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,27 +17,22 @@ class DictionaryParser {
     private final DictionaryRepository dictionaryRepository;
 
     @Transactional
-    void parse(String htmlPath) {
-        DocumentParser parser = DocumentParser.getInstance();
-        Set<String> enWords = parser.parsing(parser.read(htmlPath));
-
-        if (enWords == null) {
-            log.warn("Could not make a dictionary! english words is null.");
-            return;
-        }
-        createDictionary(parser.filtering(enWords));
+    void parse(String url) {
+        HtmlParser parser = HtmlParser.getInstance();
+        Set<String> enWords = parser.parse(parser.read(url));
+        createDictionary(enWords);
     }
 
-    private void createDictionary(Set<String> set) {
+    private void createDictionary(Set<String> enWords) {
         List<Dictionary> dictionaries = new ArrayList<>();
-        for (String enWord : set) {
-            String krWord = translator.translate(enWord)
-                .orElseThrow();
-
-            Dictionary dictionary = Dictionary.of(enWord, krWord);
-
+        for (String enWord : enWords) {
+            Optional<String> translated = translator.translate(enWord);
+            if (translated.isEmpty()) {
+                continue;
+            }
+            String krWord = translated.get();
             if (!dictionaryRepository.existsByEnWord(enWord)) {
-                dictionaries.add(dictionary);
+                dictionaries.add(Dictionary.of(enWord, krWord));
             }
         }
         dictionaryRepository.saveAll(dictionaries);
