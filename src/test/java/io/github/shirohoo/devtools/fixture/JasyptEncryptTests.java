970a29cc03ef152710dc2336bc45aabfe385b59b
shirohoo <shirohoo4102@gmail.com>
2022-03-04 13:51:39 +0900

    :fire: JasyptEncryptTests fail 보완

--- src/test/java/io/github/shirohoo/devtools/fixture/JasyptEncryptTests.java
+++ src/test/java/io/github/shirohoo/devtools/fixture/JasyptEncryptTests.java
@@ -1,18 +1,23 @@
 package io.github.shirohoo.devtools.fixture;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 
 class JasyptEncryptTests {
     @Test
     void encrypt() throws Exception {
-        String password = "";
-        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        encryptor.setAlgorithm("PBEWithMD5AndDES");
-        encryptor.setPassword(password);
+        assertThatCode(() -> {
 
-        String plainText = "";
-        String encryptedText = encryptor.encrypt(plainText);
-        System.out.println(encryptedText);
+            String password = "enter password";
+            StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+            encryptor.setAlgorithm("PBEWithMD5AndDES");
+            encryptor.setPassword(password);
+
+            String plainText = "enter target";
+            String encryptedText = encryptor.encrypt(plainText);
+            System.out.println(encryptedText);
+
+        }).doesNotThrowAnyException();
     }
 }
